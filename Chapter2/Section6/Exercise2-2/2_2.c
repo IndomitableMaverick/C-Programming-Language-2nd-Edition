0f851f657286be93e82e55ca3e7c16546b93eaa3
IndomitableMaverick <BitAsync@gmail.com>
2024-09-16 22:38:42 +0800

    new file:   Chapter2/Section6/Exercise2-2/2_2.c 	new file:   Chapter2/Section6/Exercise2-2/2_2.exe 	new file:   TODO.md

---
+++ Chapter2/Section6/Exercise2-2/2_2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+main()
+{
+    int c, i, state = 1, lim = 16;
+    char s[lim];
+
+    for (i = 0; state; ++i)
+    {
+        state = 0;
+        if(i < lim - 1)
+            if((c = getchar()) != EOF)
+                if(c != '\n')
+                {
+                    s[i] = c;
+                    state = 1;
+                }
+    }
+    s[i] = '\0';
+    printf("%s", s);
+}
