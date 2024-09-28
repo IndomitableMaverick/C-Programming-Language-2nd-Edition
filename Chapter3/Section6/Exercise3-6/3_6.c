8d776f106e7e83e726382d0437f5f594d2e8b7a9
IndomitableMaverick <BitAsync@gmail.com>
2024-09-28 18:43:31 +0800

    new file:   Chapter3/Section6/Exercise3-6/3_6.c new file:   Chapter3/Section6/Exercise3-6/3_6.exe

---
+++ Chapter3/Section6/Exercise3-6/3_6.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+
+void itoa(int n, char s[], int width) ;
+void reverse(char s[]);
+
+main()
+{
+    int n = 1 << sizeof(int) * 8 - 1;
+    char s[1024];
+    itoa(n, s, 8);
+    printf("%s\n", s);
+
+    n = 16;
+    itoa(n, s, 8);
+    printf("%s\n", s);
+}
+
+/* itoa: convert n to characters in s, the third argument is a minimum field width */ 
+void itoa(int n, char s[], int width) 
+{ 
+    int i, sign; 
+    if ((sign = n) < 0) /* record sign */ 
+        n = -n; /* make n positive */ 
+    i = 0; 
+    do { /* generate digits in reverse order */ 
+        s[i++] = n % 10 * (n % 10 < 0 ? -1 : 1) + '0'; /* get next digit */ 
+    } while ((n /= 10) != 0); /* delete it, using != 0 to handle with -(2^wordsize - 1).*/ 
+    if (sign < 0) 
+        s[i++] = '-'; 
+    while(i < width) /* padded with blanks on the left if necessary to make it wide enough. */
+        s[i++] = ' ';
+    s[i] = '\0';
+    reverse(s); 
+}
+
+void reverse(char s[])
+{
+    int i, j;
+    char c;
+    for (i = 0, j = strlen(s) - 1; i < j; i++, j--) 
+        c = s[i], s[i] = s[j], s[j] = c;
+}
