546120392af147b1ecf4f22abaa3ca1cb49fd390
IndomitableMaverick <BitAsync@gmail.com>
2024-11-07 23:28:07 +0800

    fix: handle number followed by EOF correctly in getint.c modified:   Chapter5/Section2/Exercise5-1/5_1.exe modified:   Chapter5/Section2/Exercise5-1/getint.c

--- Chapter5/Section2/Exercise5-1/getint.c
+++ Chapter5/Section2/Exercise5-1/getint.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <ctype.h>
 
+#define NUMBER_NOT_FOUND EOF
+#define NUMBER_FOUND 1
+
 int getch(void); 
 void ungetch(int); 
 
 /* getint: get next integer from input into *pn */ 
 int getint(int *pn) 
 { 
-    int c, sign; 
+    int c, sign, flag;
+    flag = NUMBER_NOT_FOUND;
 
     while (isspace(c = getch())) /* skip white space */ 
         ; 
@@ -22,10 +26,13 @@ int getint(int *pn)
             ungetch(sign == 1 ? '+' : '-');
             return 0; 
         }
-    for (*pn = 0; isdigit(c); c = getch()) 
+    for (*pn = 0; isdigit(c); c = getch()) {
         *pn = 10 * *pn + (c - '0'); 
+        flag = NUMBER_FOUND;
+    }
     *pn *= sign; 
-    if (c != EOF) 
+    if (c != EOF)
         ungetch(c); 
-    return c; 
+    else /* This ensures that if a number is followed by EOF, it is still processed correctly. */
+        return flag;
 }
