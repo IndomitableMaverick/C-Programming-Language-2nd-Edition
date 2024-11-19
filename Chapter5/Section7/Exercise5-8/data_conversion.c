ac13ab4f2b89872ae5493e5f657c1f715f995685
IndomitableMaverick <BitAsync@gmail.com>
2024-11-19 23:38:30 +0800

    modified:   Chapter5/Section7/Exercise5-8/5_8.exe modified:   Chapter5/Section7/Exercise5-8/data_conversion.c

--- Chapter5/Section7/Exercise5-8/data_conversion.c
+++ Chapter5/Section7/Exercise5-8/data_conversion.c
@@ -6,6 +6,8 @@ static char daytab[2][13] = {
 /* day_of_year: set day of year from month & day */ 
 int day_of_year(int year, int month, int day) 
 { 
+    if(year <= 0 || month <= 0 || day <= 0)
+        return -1;
     int i, leap; 
     leap = year%4 == 0 && year%100 != 0 || year%400 == 0; 
     if(month > 12 || day > daytab[leap][month])
@@ -18,6 +20,11 @@ int day_of_year(int year, int month, int day)
 /* month_day: set month, day from day of year */ 
 void month_day(int year, int yearday, int *pmonth, int *pday) 
 { 
+    if(year <= 0 || yearday <= 0) {
+        *pmonth = -1;
+        *pday = -1;
+        return;
+    }
     int i, leap; 
     leap = year%4 == 0 && year%100 != 0 || year%400 == 0; 
     for (i = 1; i < 13 && yearday > daytab[leap][i]; i++) 
